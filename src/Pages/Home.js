9ddbf8ed92a9e6f4c0575bd3afd27c67b1093bfc
nobrayn <nobrayn@gmail.com>
2023-03-26 21:07:58 -0400

    homepage HTML done for now, footer as well. replaced logo pngs with svgs.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,12 +12,21 @@ import iconBank from "../assets/icon-bank.png";
 import iconSettings from "../assets/icon-settings.png";
 import line from "../assets/line.png"
 import topTalents from "../assets/effect-circle-full.png"
-
+import beginQuote from "../assets/icon-begin-quote.png"
+import logoGoogle from "../assets/logo-google.png"
+import logoDiscord from "../assets/logo-discord.png"
+import logoDribbble from "../assets/logo-dribbble.png"
+import logoWalmart from "../assets/logo-walmart.png"
+import logoVine from "../assets/logo-vine.png"
+import logoAirbnb from "../assets/logo-airbnb.png"
+import arrowRightBig from "../assets/btn-arrow-continue-reading.png"
+import arrowDrawn from "../assets/effect-arrow.png"
+import drawnEffect from "../assets/effect-1.png"
 
 const Home = () => {
   return (
     <>
-      <section className="home-top">
+      <section id="home-top">
         <div className="home-left">
           <h1>Home time</h1>
           <p>With the largest professional creative community online, simply search through our website.</p>
@@ -69,7 +78,7 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="how-it-works">
+      <section id="how-it-works">
         <div className="hiw-heading">
           {/* replace these with border tops?? */}
           <img src={line} alt="" />
@@ -94,10 +103,10 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="new-job-listing">
+      <section id="new-job-listing">
         {/* need to set firebase data structure up here first */}
       </section>
-      <section className="find-top-talents">
+      <section id="find-top-talents">
         <div className="top-talents-left">
           <a href=""><img src={topTalents} alt="" /></a>
         </div>
@@ -110,7 +119,7 @@ const Home = () => {
           <Accordion heading="Seamless Search" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui doloremque repellendus,  magni illum dignissimos error dolore." />
         </div>
       </section>
-      <section className="top-experts">
+      <section id="top-experts">
         <div>
           <h2>Top Experts</h2>
           <div>
@@ -121,32 +130,103 @@ const Home = () => {
         <div className="experts">
           <div className="expert">
             <div className="expert-image-container">
-              <img src="https://placeimg.com/200/200/people" alt="placeholder image of a person" />
+              <img src="https://loremflickr.com/200/200/man?random=1" alt="placeholder image of a person" />
             </div>
             <p><strong>Billy Blinkers</strong></p>
             <p>Horse Whisperer</p>
           </div>
           <div className="expert">
             <div className="expert-image-container">
-              <img src="https://placeimg.com/200/200/people" alt="placeholder image of a person" />
+              <img src="https://loremflickr.com/200/200/karate?random=2" alt="placeholder image of a person" />
             </div>
             <p><strong>Candice Crackercorn</strong></p>
             <p>Kickboxer</p>
           </div>
           <div className="expert">
             <div className="expert-image-container">
-              <img src="https://placeimg.com/200/200/people" alt="placeholder image of a person" />
+              <img src="https://loremflickr.com/200/200/comedian?random=3" alt="placeholder image of a person" />
             </div>
             <p><strong>Björn Smith</strong></p>
             <p>Stand-up Comedian</p>
           </div>
           <div className="expert">
             <div className="expert-image-container">
-              <img src="https://placeimg.com/200/200/people" alt="placeholder image of a person" />
+              <img src="https://loremflickr.com/200/200/child?random=1" alt="placeholder image of a person" />
             </div>
             <p><strong>Dr. Martha Boop</strong></p>
             <p>Actual Doctor</p>
           </div>
+        </div>
+      </section>
+      <section id="feedback">
+        <div className="feedback-box-left">
+          <div className="inner-box-left">
+            <img src={beginQuote} alt="" />
+            {/* this has to be a carousel */}
+            <h3>"The best service I've ever received from Jobi, and highly recommended.</h3>
+          </div>
+          {/* where do the arrows go?! */}
+        </div>
+        <div className="feedback-box-right">
+          {/* carousel!? */}
+        </div>
+        <div className="feedback-lower">
+          <p>TRUSTED BY <span className="green-text">500+ </span>COMPANIES</p>
+          <div className="company-logos">
+            <img src={logoGoogle} alt="" />
+            <img src={logoDiscord} alt="" />
+            <img src={logoDribbble} alt="" />
+            <img src={logoWalmart} alt="" />
+            <img src={logoVine} alt="" />
+            <img src={logoAirbnb} alt="" />
+          </div>
+        </div>
+      </section>
+      <section id="guides">
+        <div>
+          <h2>Jobi Guides</h2>
+          <div>
+            <a href="#">Explore More</a>
+            <a href="#"><img src={arrowRight} alt="" /></a>
+          </div>
+        </div>
+        <div className="guides-container">
+          <div className="guides-left">
+            <div className="guides-image">
+              <img src="https://loremflickr.com/340/200/layouts?random=1" alt="placeholder image" />
+              <p className="guides-image-btn">SOLUTION</p>
+            </div>
+            <div className="guides-text">
+              <p><span className="bold">Featured - </span>18 Jul 2018</p>
+              <h4>Print, publishing qui visual quis layout mockups.</h4>
+              <a href="#">Continue Reading <img src={arrowRightBig} alt="arrow pointing right" /></a>
+            </div>
+          </div>
+          <div className="guides-right">
+            <div className="guides-image">
+              <img src="https://loremflickr.com/340/200/userexperience?random=1" alt="placeholder image" />
+              <p className="guides-image-btn">DESIGN</p>
+            </div>
+            <div className="guides-text">
+              <p>16 Jul 2018</p>
+              <h4>Designer's Checklist for Every UX/UI Project.</h4>
+              <a href="#">Continue Reading <img src={arrowRightBig} alt="arrow pointing right" /></a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="bottom-banner">
+        <div className="bottom-banner-left">
+          <h3>Complete job portal for everyone.</h3>
+          <div className="bottom-banner-btns">
+            <p>Looking for a job?</p>
+            <p>Post a job</p>
+            <img src={arrowDrawn} alt="arrow pointing left" />
+          </div>
+        </div>
+        <div className="bottom-banner-right">
+          <img src="https://loremflickr.com/200/200/job?random=1" alt="placeholder image" />
+          <img src={drawnEffect} alt="emphasis lines" />
         </div>
       </section>
     </>
